1dad95898b06e6e939ecc47bb4278a1df2f1899b
DaWaildHorse <andeelou69@outlook.com>
2025-08-06 15:16:34 -0600

    wip: worked on Knowledge Graph nodes creation

--- knowledgeGraph.py
+++ knowledgeGraph.py
@@ -5,12 +5,22 @@ import getpass
 from langchain_google_genai.llms import GoogleGenerativeAI
 from langchain_experimental.graph_transformers import LLMGraphTransformer
 from langchain_community.graphs import Neo4jGraph
+from langchain.schema import Document
+
 
 
 loader = TextLoader("output.txt")
 documents= loader.load()
+
+full_text = "".join([doc.page_content for doc in documents])
+
+# Take the first 3000 characters, need to see if can be increased
+sample_text = full_text[:3000]
+
+sample_doc = [Document(page_content=sample_text)]
 text_splitter = CharacterTextSplitter(chunk_size=200 , chunk_overlap= 20)
-texts = text_splitter.split_documents(documents)
+texts = text_splitter.split_documents(sample_doc)
+
 
 os.environ["GOOGLE_API_KEY"] = getpass.getpass()
 
